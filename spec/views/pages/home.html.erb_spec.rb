c829161c4596c93504eb8a86f28ba1a90e4194e6
ZjanPreijde <j.preijde2@upcmail.nl>
2018-01-21 18:35:30 +0100

    Desperately trying to get Rspec/Capybara to work, getting error on 'visit root_url'

---
+++ spec/views/pages/home.html.erb_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "pages/home.html.erb", type: :view do
+  it "should return Rails default text, but can not get this to work"
+  it "it returns NoMethodError: undefined method `visit' for #<RSpec::ExampleGroups::PagesHomeHtml..."
+  # do
+  #   visit root_url
+  #   expect(page).to have_content "Pages#home"
+  # end
+end
